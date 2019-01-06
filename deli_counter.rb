4efaf17d965311dceeea8ca354bbaeabf670e2fc
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-01-06 23:26:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 def line(katz_deli)
   if katz_deli.empty? 
     puts "The line is currently empty."
-  else katz_deli(customers)
+  else
     puts "The line is currently:"
     katz_deli.each_with_index(1) do |customer, index| current_line <<  " #{index}. #{customer}"}
   end
