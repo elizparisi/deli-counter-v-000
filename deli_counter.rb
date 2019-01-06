e6cfeb50d9dff434b8411a7bb498c7cbb99f0976
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-01-06 23:26:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ def line(katz_deli)
     puts "The line is currently empty."
   elsif katz_deli(customers)
     puts "The line is currently:"
-    katz_deli.each_with_index(1) do |customer, index| katz_deli <<  " #{index + 1}. #{customer}"}
+    katz_deli.each_with_index(1) do |customer, index| current_line <<  " #{index}. #{customer}"}
   end
-    puts katz_deli
+    puts current_line
 end
 
 def take_a_number(katz_deli, new_customer)
